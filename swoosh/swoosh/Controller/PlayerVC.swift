680077121f938f46b04d9fadd5ab5258d731308e
kiamalek <44207170+kiamalek@users.noreply.github.com>
2020-09-01 22:45:43 +0430

    I focused on it more thamn 2 days

--- swoosh/swoosh/Controller/PlayerVC.swift
+++ swoosh/swoosh/Controller/PlayerVC.swift
@@ -9,28 +9,68 @@
 import UIKit
 
 class PlayerVC: UIViewController {
-
+    
     @IBOutlet weak var kyli: BoarderButton!
     @IBOutlet weak var Ronaldo: UIImageView!
     @IBOutlet weak var womanTeam: BoarderButton!
     @IBOutlet weak var menTeam: BoarderButton!
+    @IBOutlet weak var nextbtn: BoarderButton!
     var selectedSkill : String!
+    var selectedChar :String!
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(selectedSkill)
         print("welcome to this page!")
         print("again")
         print("again")
     }
     
     @IBAction func nextBtn(_ sender: Any) {
-        
-    }
-    @IBAction func menTeam(_ sender: Any) {
-    }
-    
+        nextbtn.isEnabled = true
+        if selectedChar == "men" {
+            print("You can open chiristian Ronaldo page")
+        }
+        else if  selectedChar == "Women" {
+            print("You prefer open Alex Morgab page")
+        }
+        else if selectedChar == "coRed" {
+            print("It's coRed")
+        }
+    else if selectedChar == "men" {
+            selectedChar = "men"
+            nextbtn.isEnabled = true
+        }
+        }
+    @IBAction func nextBtn(_ sender: Any) {
+            nextbtn.isEnabled = true
+            if selectedChar == "woman" {
+                print("Open Alex Morgan page")
+        }
+            else if selectedChar == "men" {
+                print("You can open Cristian Ronaldo page")
+        }
+            else if selectedChar == "coRed" {
+                print("This is coRed")
+        }
+        @IBAction func nextBtn(_ sender: Any) {
+        nextbtn.isEnabled = true
+        if selectedChar == "coRed" {
+            print("Both gender play this sport")
+        }
+        else if selectedChar == "men" {
+            print("It's not only for men")
+        }
+        else if selectedChar == "women" {
+            print("It's not just for women"
+            )
+            }
+                
+                
+            }
+            
+            
+            
+            
+            
+}
 
-    @IBAction func wommanTeam(_ sender: Any) {
-    }
-    
 }
